f6a0f918d3227949558c3a60429bfda7d7d539c1
tauanebezerra <tauanebezerra@gmail.com>
2022-08-07 00:40:17 -0300

    Criando cards animal e depoimento

---
+++ src/assets/shared/components/card-depoimento/card-depoimento.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardDepoimentoComponent } from './card-depoimento.component';
+
+describe('CardDepoimentoComponent', () => {
+  let component: CardDepoimentoComponent;
+  let fixture: ComponentFixture<CardDepoimentoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardDepoimentoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardDepoimentoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
