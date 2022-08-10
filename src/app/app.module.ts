96631b36aa70b7850cdc7682fe921df6f0b674b2
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-08-10 15:42:54 -0300

    Alterando o styles.scss - cores dos botoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FaqComponent } from './faq/faq.component';
 import { ContatoComponent } from './contato/contato.component';
 import { QueroDoarComponent } from './quero-doar/quero-doar.component';
+
+import { CadastroUsuarioModule } from './cadastro-usuario/cadastro-usuario.module';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { QueroDoarComponent } from './quero-doar/quero-doar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    CadastroUsuarioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
