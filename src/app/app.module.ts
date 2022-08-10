bb62a5e86f9964448e838ab6eec3a817825e3319
deisealves <102121940+deisealves@users.noreply.github.com>
2022-08-09 23:37:34 -0300

    adicionando novos componentes
    contato, faq, quero doar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContatoComponent } from './contato/contato.component';
+import { QueroDoarComponent } from './quero-doar/quero-doar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    ContatoComponent,
+    QueroDoarComponent
   ],
   imports: [
     BrowserModule,
