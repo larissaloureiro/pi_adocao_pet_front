87351384aefd892d2961677fc1622845a7f8eb2b
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-08-12 15:32:11 -0300

    Alterações no card-animal

--- src/assets/shared/components/card-animal/card-animal.component.ts
+++ src/assets/shared/components/card-animal/card-animal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AnimalService } from 'src/app/service/animal.service';
 
 @Component({
   selector: 'app-card-animal',
@@ -6,24 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card-animal.component.scss']
 })
 export class CardAnimalComponent implements OnInit {
-
   @Input()
-  nomeAnimal: string;
-
-  @Input()
-  descricaoAnimal: string;
-
-  @Input()
-  link: string;
-
-  @Input()
-  imagemAnimal: string;
-
-  @Input()
-  background: string;
+  animal: any;
 
   constructor() { }
-
 
   ngOnInit(): void {
   }
