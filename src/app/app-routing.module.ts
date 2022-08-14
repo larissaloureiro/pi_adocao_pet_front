010052a07254b5e9f155d52cc05431502fd19995
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-08-14 20:38:41 -0300

    Ajustando o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { CadastroAnimalComponent } from './cadastro-animal/cadastro-animal.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AreaUsuarioComponent } from './area-usuario/area-usuario.component';
@@ -13,9 +15,12 @@ import { QueroDoarComponent } from './quero-doar/quero-doar.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastro', component: CadastroUsuarioComponent },
+  { path: 'cadastro-animal', component: CadastroAnimalComponent},
   { path: 'contato', component: ContatoComponent },
+  { path: 'detalhe-animal/:id', component: DetalheAnimalComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'lista-animais', component: ListaAnimaisComponent },
+  { path: 'lista-animais/:especie', component: ListaAnimaisComponent },
   { path: 'login', component: LoginComponent },
   { path: 'quem-somos', component: QuemSomosComponent },
   { path: 'quero-doar', component: QueroDoarComponent },
