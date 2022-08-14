07e7094a64a5e64d7f94cafc17e9776e0d419a1f
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-08-14 20:35:11 -0300

    Ajustando filtro de animais

--- src/assets/shared/components/filtro/filtro.component.ts
+++ src/assets/shared/components/filtro/filtro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filtro',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filtro.component.scss']
 })
 export class FiltroComponent implements OnInit {
+  especie: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.router.navigate(['lista-animais/' + this.especie]);
+  }
 }
--- src/assets/shared/components/filtro/filtro.module.ts
+++ src/assets/shared/components/filtro/filtro.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FiltroComponent } from './filtro.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [FiltroComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     FiltroComponent
   ]
-
-  
 })
 export class FiltroModule { }
