47e0ea061de6ef275b12db1c4277f0ab78cdfc5d
Larissa Loureiro <larissa.loureiro@ymail.com>
2022-08-17 16:07:49 -0300

    Ajustes para o Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlAuth: "https://pi-adocao-pet-back.herokuapp.com/auth/signin",
+  urlAnimal: "https://pi-adocao-pet-back.herokuapp.com/api/animal/v1/",
+  urlUsuario: "https://pi-adocao-pet-back.herokuapp.com/api/usuario/v1"
 };
